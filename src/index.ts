865d186174295e4ee2200fd0cb16a1f0de00d060
Kushagra Singhal <singhalkushagra21@gmail.com>
2025-07-18 03:37:04 +0530

    added room logic

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,62 @@
 import { WebSocket, WebSocketServer } from 'ws';
-import { createServer } from 'http';
-
 
 const wss = new WebSocketServer({ port: 8080 });
 
 const rooms = new Map<string, WebSocket[]>();
+const socketRoom = new WeakMap<WebSocket, string>();
 
-let allSockets: WebSocket[] = [];
 wss.on('connection', (ws) => {
-  allSockets.push(ws);
+  
   console.log('Client connected');
-  ws.on('message', (message: string) => {
-    let parsedmsg = JSON.parse(message)
-    if (parsedmsg.type === 'join') {
-      const room = parsedmsg.room;
-      if (!rooms.has(room)) {
-        rooms.set(room, []);
-      }
-      rooms.get(room)?.push(ws);
-      ws.send(`Joined room: ${room}`);
-    } else if (parsedmsg.type === 'leave') {
-      const room = parsedmsg.room;
-      if (rooms.has(room)) {
-        const roomSockets = rooms.get(room);
-        if (roomSockets) {
-          rooms.set(room, roomSockets.filter((socket) => socket !== ws));
-          ws.send(`Left room: ${room}`);
+
+  ws.on('message', (data) => {
+    try {
+      const message = typeof data === 'string' ? data : data.toString();
+      const parsedMsg = JSON.parse(message);
+
+      if (parsedMsg.type === 'join') {
+        const room = parsedMsg.room;
+        if (!rooms.has(room)) {
+          rooms.set(room, []);
         }
-      }}
-    allSockets.forEach((socket) => {
-      if (socket.readyState === WebSocket.OPEN) {
-        socket.send(`Broadcast: ${message}`);
+        rooms.get(room)!.push(ws);
+        ws.send(`Joined room: ${room}`);
+      } else if (parsedMsg.type === 'leave') {
+        const room = parsedMsg.room;
+        if (rooms.has(room)) {
+          const roomSockets = rooms.get(room);
+          if (roomSockets) {
+            rooms.set(room, roomSockets.filter((socket) => socket !== ws));
+            ws.send(`Left room: ${room}`);
+          }
+        }
       }
-    });
+
+      // Broadcast to all connected sockets
+      const room = socketRoom.get(ws);
+      if (room && parsedMsg.room) {
+        
+        const roomSockets = rooms.get(parsedMsg.room);
+        if (roomSockets) {
+          roomSockets.forEach((socket) => {
+            if (socket !== ws && socket.readyState === WebSocket.OPEN) {
+              socket.send(JSON.stringify(parsedMsg));
+            }
+          });
+        }
+      }
+
+    } catch (err) {
+      console.error('Failed to process message:', err);
+      ws.send('Invalid message format');
+    }
   });
 
   ws.on('close', () => {
-    console.log('Client disconnected');
-    allSockets = allSockets.filter((socket) => socket !== ws);
-  });
-});
+    const room = socketRoom.get(ws);
+    if (room && rooms.has(room)) {
+      rooms.set(room, rooms.get(room)!.filter((socket) => socket !== ws));
+    }
+
+    socketRoom.delete(ws);
+})});
