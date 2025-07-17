b929df710f48fd315275c6205d12fa4f3e2aae19
Kushagra Singhal <singhalkushagra21@gmail.com>
2025-07-18 03:12:43 +0530

    added room logic

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,33 @@
 import { WebSocket, WebSocketServer } from 'ws';
 import { createServer } from 'http';
+
+
 const wss = new WebSocketServer({ port: 8080 });
+
+const rooms = new Map<string, WebSocket[]>();
 
 let allSockets: WebSocket[] = [];
 wss.on('connection', (ws) => {
   allSockets.push(ws);
   console.log('Client connected');
-  ws.on('message', (message) => {
-    console.log(`Received message: ${message}`);
+  ws.on('message', (message: string) => {
+    let parsedmsg = JSON.parse(message)
+    if (parsedmsg.type === 'join') {
+      const room = parsedmsg.room;
+      if (!rooms.has(room)) {
+        rooms.set(room, []);
+      }
+      rooms.get(room)?.push(ws);
+      ws.send(`Joined room: ${room}`);
+    } else if (parsedmsg.type === 'leave') {
+      const room = parsedmsg.room;
+      if (rooms.has(room)) {
+        const roomSockets = rooms.get(room);
+        if (roomSockets) {
+          rooms.set(room, roomSockets.filter((socket) => socket !== ws));
+          ws.send(`Left room: ${room}`);
+        }
+      }}
     allSockets.forEach((socket) => {
       if (socket.readyState === WebSocket.OPEN) {
         socket.send(`Broadcast: ${message}`);
